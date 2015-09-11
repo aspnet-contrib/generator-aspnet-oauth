cddc83478916323db41c2f962c26ab7fbd410258
Jerrie Pelser <jerrie@jerriepelser.com>
2015-09-11 12:34:22 +0700

    Add initial generator

---
+++ generators/app/templates/AuthenticationOptions.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/aspnet-contrib/AspNet.Security.OAuth.Providers
+ * for more information concerning the license and the contributors participating to this project.
+ */
+
+using Microsoft.AspNet.Authentication.OAuth;
+using Microsoft.AspNet.Http;
+
+namespace AspNet.Security.OAuth.<%= name %> {
+    /// <summary>
+    /// Defines a set of options used by <see cref="<%= name %>AuthenticationHandler"/>.
+    /// </summary>
+    public class <%= name %>AuthenticationOptions : OAuthAuthenticationOptions {
+        public <%= name %>AuthenticationOptions() {
+            AuthenticationScheme = <%= name %>AuthenticationDefaults.AuthenticationScheme;
+            Caption = <%= name %>AuthenticationDefaults.Caption;
+            ClaimsIssuer = <%= name %>AuthenticationDefaults.Issuer;
+
+            CallbackPath = new PathString(<%= name %>AuthenticationDefaults.CallbackPath);
+
+            AuthorizationEndpoint = <%= name %>AuthenticationDefaults.AuthorizationEndpoint;
+            TokenEndpoint = <%= name %>AuthenticationDefaults.TokenEndpoint;
+            UserInformationEndpoint = <%= name %>AuthenticationDefaults.UserInformationEndpoint;
+
+            SaveTokensAsClaims = false;
+        }
+    }
+}
