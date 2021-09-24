5923d62771abccf6446ea8fbe5ea1ab048eed527
martincostello <martin@martincostello.com>
2021-09-24 12:28:28 +0100

    Access Events directly
    Access the Events property directly, rather than via Options, so the correct value is used if EventsType is set.

--- generators/app/templates/AuthenticationHandler.cs
+++ generators/app/templates/AuthenticationHandler.cs
@@ -75,7 +75,7 @@ namespace AspNet.Security.OAuth.<%= name %>
             var context = new OAuthCreatingTicketContext(principal, properties, Context, Scheme, Options, Backchannel, tokens, payload.RootElement);
             context.RunClaimActions();
 
-            await Options.Events.CreatingTicket(context);
+            await Events.CreatingTicket(context);
             return new AuthenticationTicket(context.Principal!, context.Properties, Scheme.Name);
         }
     }
