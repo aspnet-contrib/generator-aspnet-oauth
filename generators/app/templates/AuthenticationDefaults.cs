a36e21e2240cc23d089008dd6079b49607d6d33f
martincostello <martin@martincostello.com>
2021-09-25 12:21:35 +0100

    Use file-scoped namespaces
    Switch to using file-scoped namespaces.
    Remove using statements covered by global using statements.

--- generators/app/templates/AuthenticationDefaults.cs
+++ generators/app/templates/AuthenticationDefaults.cs
@@ -4,49 +4,45 @@
  * for more information concerning the license and the contributors participating to this project.
  */
 
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.OAuth;
+namespace AspNet.Security.OAuth.<%= name %>;
 
-namespace AspNet.Security.OAuth.<%= name %>
+/// <summary>
+/// Default values used by the <%= name %> authentication provider.
+/// </summary>
+public static class <%= name %>AuthenticationDefaults
 {
     /// <summary>
-    /// Default values used by the <%= name %> authentication provider.
+    /// Default value for <see cref="AuthenticationScheme.Name"/>.
     /// </summary>
-    public static class <%= name %>AuthenticationDefaults
-    {
-        /// <summary>
-        /// Default value for <see cref="AuthenticationScheme.Name"/>.
-        /// </summary>
-        public const string AuthenticationScheme = "<%= name %>";
+    public const string AuthenticationScheme = "<%= name %>";
 
-        /// <summary>
-        /// Default value for <see cref="AuthenticationScheme.DisplayName"/>.
-        /// </summary>
-        public static readonly string DisplayName = "<%= name %>";
+    /// <summary>
+    /// Default value for <see cref="AuthenticationScheme.DisplayName"/>.
+    /// </summary>
+    public static readonly string DisplayName = "<%= name %>";
 
-        /// <summary>
-        /// Default value for <see cref="AuthenticationSchemeOptions.ClaimsIssuer"/>.
-        /// </summary>
-        public static readonly string Issuer = "<%= name %>";
+    /// <summary>
+    /// Default value for <see cref="AuthenticationSchemeOptions.ClaimsIssuer"/>.
+    /// </summary>
+    public static readonly string Issuer = "<%= name %>";
 
-        /// <summary>
-        /// Default value for <see cref="RemoteAuthenticationOptions.CallbackPath"/>.
-        /// </summary>
-        public static readonly string CallbackPath = "/signin-<%= name.toLowerCase() %>";
+    /// <summary>
+    /// Default value for <see cref="RemoteAuthenticationOptions.CallbackPath"/>.
+    /// </summary>
+    public static readonly string CallbackPath = "/signin-<%= name.toLowerCase() %>";
 
-        /// <summary>
-        /// Default value for <see cref="OAuthOptions.AuthorizationEndpoint"/>.
-        /// </summary>
-        public static readonly string AuthorizationEndpoint = "<%= authorizationendpoint %>";
+    /// <summary>
+    /// Default value for <see cref="OAuthOptions.AuthorizationEndpoint"/>.
+    /// </summary>
+    public static readonly string AuthorizationEndpoint = "<%= authorizationendpoint %>";
 
-        /// <summary>
-        /// Default value for <see cref="OAuthOptions.TokenEndpoint"/>.
-        /// </summary>
-        public static readonly string TokenEndpoint = "<%= tokenendpoint %>";
+    /// <summary>
+    /// Default value for <see cref="OAuthOptions.TokenEndpoint"/>.
+    /// </summary>
+    public static readonly string TokenEndpoint = "<%= tokenendpoint %>";
 
-        /// <summary>
-        /// Default value for <see cref="OAuthOptions.UserInformationEndpoint"/>.
-        /// </summary>
-        public static readonly string UserInformationEndpoint = "<%= userinformationendpoint %>";
-    }
+    /// <summary>
+    /// Default value for <see cref="OAuthOptions.UserInformationEndpoint"/>.
+    /// </summary>
+    public static readonly string UserInformationEndpoint = "<%= userinformationendpoint %>";
 }
